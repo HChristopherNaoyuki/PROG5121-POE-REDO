947a8d60a609f772fc1d00b655da63ab4442e061
Naoyuki Christopher <165992578+HChristopherNaoyuki@users.noreply.github.com>
2024-10-20 15:31:09 +0200

    Update Registration.java

--- src/registration/Registration.java
+++ src/registration/Registration.java
@@ -1,7 +1,4 @@
 /*
- * Author: Naoyuki Christopher Higaki
- * Student Number: ST10462415
- * 
  * This program is written by Naoyuki Christopher Higaki.
  * Purpose: This class serves as the main entry point 
  * for the registration and login application, including task management features.
