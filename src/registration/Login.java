b495eb4a78535c635019f3af0eeec1826e5bc9a1
Naoyuki Christopher <165992578+HChristopherNaoyuki@users.noreply.github.com>
2024-10-20 15:30:56 +0200

    Update Login.java

--- src/registration/Login.java
+++ src/registration/Login.java
@@ -1,7 +1,4 @@
 /*
- * Author: Naoyuki Christopher Higaki
- * Student Number: ST10462415
- * 
  * This program is written by Naoyuki Christopher Higaki.
  * Purpose: This class provides functionality for user registration and login, including username and password validation.
  */
